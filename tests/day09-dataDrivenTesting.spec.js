1085c8f728f10172c30a97cbb062382c2803a732
Jester <asoy.jestern@gmail.com>
2026-07-07 23:04:38 +0800

    Day 09 Data Driven Testing

---
+++ tests/day09-dataDrivenTesting.spec.js
@@ -0,0 +1,26 @@
+//Day 9 - What is Data-Driven Testing?
+
+import { test, expect } from "@playwright/test";
+
+const LOGIN_URL = "https://practicetestautomation.com/practice-test-login/"
+
+const loginData = [
+    {
+        username: "student",
+        password: "Password123"
+    },
+]
+
+test("Valid Login", async ({ page }) => {
+
+    await page.goto(LOGIN_URL);
+
+    const username = page.getByLabel("Username");
+    const password = page.getByLabel("Password");
+    const submit = page.getByRole("button", { name: "Submit" })
+
+    await username.fill(loginData[0].username)
+    await password.fill(loginData[0].password)
+    await submit.click();
+
+})
