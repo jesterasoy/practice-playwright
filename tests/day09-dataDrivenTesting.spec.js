15d8ee1d22a257c6fed86aa0f301f7b5dfb272c8
Jester <asoy.jestern@gmail.com>
2026-07-08 23:42:03 +0800

    Day 09 Data Driven Testing Part 2

--- tests/day09-dataDrivenTesting.spec.js
+++ tests/day09-dataDrivenTesting.spec.js
@@ -9,18 +9,42 @@ const loginData = [
         username: "student",
         password: "Password123"
     },
+    {
+        username: "pogi",
+        password: "WrongPassword"
+    }
 ]
 
-test("Valid Login", async ({ page }) => {
+for (const user of loginData) {
 
-    await page.goto(LOGIN_URL);
+    test(`Login with ${user.username}`, async ({ page }) => {
 
-    const username = page.getByLabel("Username");
-    const password = page.getByLabel("Password");
-    const submit = page.getByRole("button", { name: "Submit" })
+        await page.goto(LOGIN_URL);
 
-    await username.fill(loginData[0].username)
-    await password.fill(loginData[0].password)
-    await submit.click();
+        const username = page.getByLabel("Username");
+        const password = page.getByLabel("Password");
+        const submit = page.getByRole("button", { name: "Submit" });
 
-})
+        await username.fill(user.username);
+        await password.fill(user.password);
+        await submit.click();
+
+    });
+}
+
+
+
+
+// test("Valid Login", async ({ page }) => {
+
+//     await page.goto(LOGIN_URL);
+
+//     const username = page.getByLabel("Username");
+//     const password = page.getByLabel("Password");
+//     const submit = page.getByRole("button", { name: "Submit" })
+
+//     await username.fill(loginData[0].username)
+//     await password.fill(loginData[0].password)
+//     await submit.click();
+
+// })
