6c07c79e6646b00ca7ac83273f3fd037f35d6bbe
Jester <asoy.jestern@gmail.com>
2026-07-21 15:12:40 +0800

    session token

--- tests/auth/auth.setup.spec.js
+++ tests/auth/auth.setup.spec.js
@@ -5,7 +5,7 @@ test("Authenticate User", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
 
   await page.locator("#user-name").fill("standard_user");
-  await page.locator("#password").fill("secret_sauc ");
+  await page.locator("#password").fill("secret_sauce");
   await page.getByRole("button", { name: "Login" }).click();
 
   await page.context().storageState({
