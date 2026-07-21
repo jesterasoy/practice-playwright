ca9dba3abda4047e9d7ab5e634660cf04987da49
Jester <asoy.jestern@gmail.com>
2026-07-21 16:58:17 +0800

    session auth

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,41 +1,45 @@
 import { expect } from "@playwright/test";
 
 export class LoginPage {
-    constructor(page) {
-        this.page = page;
-        this.username = page.getByLabel("Username");
-        this.password = page.getByLabel("Password");
-        this.submit = page.getByRole("button", { name: "Submit" });
-        this.success = page.getByRole("heading", {
-            name: "Logged In Successfully"
-        }); this.error = page.locator("#error", { name: "Your password is invalid!" })
-        this.logout = page.getByRole('link', { name: 'Logout' })
+  constructor(page) {
+    this.page = page;
+    this.username = page.getByLabel("Username");
+    this.password = page.getByLabel("Password");
+    this.submit = page.getByRole("button", { name: "Submit" });
+    this.success = page.getByRole("heading", {
+      name: "Logged In Successfully",
+    });
+    this.error = page.locator("#error", { name: "Your password is invalid!" });
+    this.logout = page.getByRole("link", { name: "Logout" });
+  }
 
-    }
+  async goto() {
+    await this.page.goto(
+      "https://practicetestautomation.com/practice-test-login/",
+    );
+  }
 
-    async goto() {
-        await this.page.goto("https://practicetestautomation.com/practice-test-login/")
-    }
+  async login(username, password) {
+    await this.username.fill(username);
+    await this.password.fill(password);
+    await this.submit.click();
+  }
 
-    async login(username, password) {
-        await this.username.fill(username)
-        await this.password.fill(password)
-        await this.submit.click();
-    }
+  async verifySuccessfulLogin() {
+    await expect(this.success).toBeVisible();
+  }
 
-    async verifySuccessfulLogin() {
-        await expect(this.success).toBeVisible();
-    }
+  async verifyInvalidLogin() {
+    await expect(this.error).toBeVisible();
+  }
 
-    async verifyInvalidLogin() {
-        await expect(this.error).toBeVisible();
-    }
+  async logout() {
+    await this.logout.click();
+  }
 
-    async logout() {
-        await this.logout.click()
-    }
-
-    async successLogout() {
-        await expect(page).toHaveUrl("https://practicetestautomation.com/practice-test-login/")
-    }
+  async successLogout() {
+    await expect(page).toHaveUrl(
+      "https://practicetestautomation.com/practice-test-login/",
+    );
+  }
 }
